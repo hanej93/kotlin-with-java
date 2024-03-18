c940ff1dd101ca3ca56754a0d3b184f92824d64c
EJ <hanej93@naver.com>
2024-03-19 00:37:42 +0900

    1강. 코틀린에서 변수를 다루는 방법

---
+++ src/main/kotlin/com/lannstark/lec01/Lec01Main.kt
@@ -0,0 +1,11 @@
+package com.lannstark.lec01
+
+fun main() {
+    var number1 = 10L
+    val number2 = 10L
+
+    var number3: Long? = 1_000L
+    number3 = null
+
+    var person = Person("한의진")
+}
