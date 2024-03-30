5f760c704c4f5eae8ba7aed19f13a462b7ba96a9
EJ <hanej93@naver.com>
2024-03-30 19:42:41 +0900

    7강. 코틀린에서 예외를 다루는 방법

--- src/main/java/com/lannstark/lec07/Lec07Main.java
+++ src/main/java/com/lannstark/lec07/Lec07Main.java
@@ -1,10 +1,14 @@
 package com.lannstark.lec07;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Lec07Main {
 
   public static void main(String[] args) throws IOException {
+    File currentFile = new File(".");
+    JavaFilePrinter javaFilePrinter = new JavaFilePrinter();
+    javaFilePrinter.readFile(currentFile.getAbsolutePath() + "/a.txt");
   }
 
 }
